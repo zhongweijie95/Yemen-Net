c21c0b2b148a3c997cec4a12d663fa7c9b7ac917
Osama Mohammed Al-zabidi <57198110+omamkaz@users.noreply.github.com>
2024-09-21 05:47:44 +0300

    Add files via upload

--- lib/models/base.py
+++ lib/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import sqlite3
 from pydal import DAL, Field
 from datetime import datetime
 
@@ -15,13 +16,16 @@ class DBEngine:
 
     @classmethod
     def init_tables(cls):
-        DBEngine.db.define_table(
-            'users',
-            Field('atype', 'integer', default=0),  # 0: ADSL, 1: 4G LTE, 2: Phone
-            Field('username', 'string', length=32, required=True),
-            Field('password', 'string', length=32),  # Just for '0' atype
-            Field('dname', 'string', length=32),
-            Field('data', 'json'),  # Dictionary-like structure
-            Field('cookies', 'json'),  # Just for '0' atype
-            Field('created', 'datetime', default=datetime.now()),  # Current datetime
-        )
+        try:
+            DBEngine.db.define_table(
+                'users',
+                Field('atype', 'integer', default=0),  # 0: ADSL, 1: 4G LTE, 2: Phone
+                Field('username', 'string', length=32, required=True),
+                Field('password', 'string', length=32),  # Just for '0' atype
+                Field('dname', 'string', length=32),
+                Field('data', 'json'),  # Dictionary-like structure
+                Field('cookies', 'json'),  # Just for '0' atype
+                Field('created', 'datetime', default=datetime.now()),  # Current datetime
+            )
+        except sqlite3.OperationalError:
+            pass
