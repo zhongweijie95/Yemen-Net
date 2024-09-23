fa3cd9ddbff9b3dd12519c819fbb15fdb76367a3
Osama Mohammed Al-zabidi <57198110+omamkaz@users.noreply.github.com>
2024-09-23 18:12:07 +0300

    Delete lib directory

--- lib/scrapper/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-from .lte import LTE
-from .adsl import ADSL
-from .phone import Phone
-from .base import Base, ParserError
--- lib/views/card/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-from .card import Card
-from .item import CardItem
-from .credit import CardCredit
-from .list_tile import CardTitle
